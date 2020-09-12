155e7eebb15c5b27c57b45932b433c5d0e54020b
yanbutan <ynaung6995@gmail.com>
2020-09-12 16:12:26 +0800

    added content and made some css changes.Needa look at cleaning up some readmes, adding more projects and learning about scss

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import PortfolioContext from '../../context/context';
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
-  const { title, name, subtitle, cta } = hero;
+  const { title, name, subtitle, cta, pta } = hero;
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
@@ -26,16 +26,21 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'} <span className="text-color-main">{name || 'Yan'}</span>
+            {title} <span className="text-color-main">{name}</span>
             <br />
-            {subtitle || "I'm the Best Developer."}
+            {subtitle}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
           <p className="hero-cta">
             <span className="cta-btn cta-btn--hero">
               <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
+                {cta}
+              </Link>
+            </span>
+            <span className="cta-btn cta-btn--hero" style={{ marginLeft: '8px' }}>
+              <Link to="projects" smooth duration={1000}>
+                {pta}
               </Link>
             </span>
           </p>
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,55 +2,62 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Yan', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  title: 'Hi, my name is',
+  name: 'Yan',
+  subtitle: `I'm an aspiring software engineer.`,
+  cta: 'About me',
+  pta: 'Projects',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: `I'm a final year Information Systems student at the National University of Singapore. 
+  I enjoy learning new technology and applying them through internship, school or personal projects. My interests currently lies in
+  web development and the projects listed below are a testament to this.`,
+  paragraphTwo: `Apart from development, I enjoy playing floorball, jamming to songs on my guitar, learning new languages and exploring new cultures.
+                 I used to play for a floorball club with my friends and did occasional performances on my guitar for school events.`,
+  paragraphThree: `Check out my resume and reach out to me if you like what you see!`,
+  resume: 'https://drive.google.com/file/d/1oqAeZzzeBv9fhlGpAS3nCB6gDRjh6VCZ/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'bifrost.png',
+    title: 'Bifrost Dataset Search',
+    info:
+      'A dataset consolidation site that makes it easy for data scientists, developers and engineers everywhere to find the data you need!',
+    info2: `Web development project that I did for my summer internship company, Bifrost AI.`,
+    url: 'https://datasets.bifrost.ai',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'prupru.png',
+    title: 'Pruaffinity',
+    info: 'An all in one platform for life insurance purchase and management.',
+    info2:
+      'Capstone project I worked on with 5 other teammates and an external client over a period of 4 months. To try out the live site, use these credentials , username: sylvest and password: password.',
+    url: 'https://kepston.herokuapp.com',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
+    img: 'gistomondo.png',
+    title: 'Gistomondo',
+    info:
+      'Simple web application listing all of the latest gists from github. User can also see his or her own gists by clicking on My Gists on the header and logging in through github.',
+    info2: 'A take home assignment that I did for an internship interview.',
+    url: 'https://gistomondo.netlify.app',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
   },
 ];
@@ -59,7 +66,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'ynaung6995@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,23 +74,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'linkedin.com/in/yan-nyein-aung-025a15184',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/yanbutan',
     },
   ],
 };
